c0aee6c0554f729f4f845cfe327ea8a7c3cab939
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-08-27 08:08:59 +0200

    feat(conventions): add pagingoptions

--- TemporalTables/Extensions/TemporalTablesGraphQLConventions.cs
+++ TemporalTables/Extensions/TemporalTablesGraphQLConventions.cs
@@ -5,9 +5,13 @@ namespace TemporalTables.Extensions;
 public static class TemporalTablesGraphQLConventions
 {
     public static IRequestExecutorBuilder AddGraphQLConventions(this IRequestExecutorBuilder builder)
-    => builder
+        => builder
             .AddFiltering()
             .AddProjections()
             .AddSorting()
-            .AddPagingArguments();
+            .ModifyPagingOptions(o =>
+            {
+                o.DefaultPageSize = 2;
+                o.RequirePagingBoundaries = false;
+            });
 }
